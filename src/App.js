574d0213938daafbd775205a2f2c6f590669508a
walidesoussi <eoussi.walid@gmail.com>
2022-02-03 15:01:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Profile from './profile/Profile';
+import NameForm from './profile/Profile';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+        
+<Profile/>
+     
+    </div>
+  );
+}
+
+export default App;
